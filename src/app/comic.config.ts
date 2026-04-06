28901361131142c96f3b9f5591b10547a8f859f3
ilgo0413 <iliagolovin@gmail.com>
2026-04-06 21:32:18 +0200

    Set chapter 1 pages to 4

--- src/app/comic.config.ts
+++ src/app/comic.config.ts
@@ -5,7 +5,7 @@ export interface ChapterConfig {
 
 export const COMIC_CONFIG = {
   chapters: [
-    { title: 'ГЛАВА 1: Заражение', pages: 5 },
+    { title: 'ГЛАВА 1: Заражение', pages: 4 },
     { title: 'ГЛАВА 2: Дом', pages: 0 },
     { title: 'ГЛАВА 3: Корни', pages: 0 },
   ] as ChapterConfig[],
